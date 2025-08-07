f12744926dd786f51163e713dbc003bb62ccc678
sk1llahh <sk1llahh2@gmail.com>
2025-08-07 17:53:56 +0500

    initial

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,17 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: import.meta.env.VITE_API_BASE_URL,
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
